12a7a6e97f918a6b2a7656999d54f63e89075bde
Natalia Byzova <nata22011997@gmail.com>
2023-10-11 22:51:44 +0300

    feat: add second solution for virtual scroll

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Posts
+
+## About
+
+List of posts with virtual scroll as a testing task.
+
+## Key Features
+
+- Virtual scroll
+- FSD-architecture
+
+## How to try it?
+
+👉 The project is hosted on Netlify: https://posts-list-verbena.netlify.app/
+
+## Stack
+
+- React
+- React Router v6
+- Redux Toolkit
+- TypeScript
+- Sass
+- react-window
+
+## Setup and Running
+
+1. Install dependencies.
+
+```bash
+$ npm install
+```
+
+2. Run the app in the development mode.
+
+```bash
+$ npm start
+```
+
+3. Build the app for production to the `build` folder.
+
+```bash
+$ npm run build
+```
--- src/widgets/PostsList/index.tsx
+++ src/widgets/PostsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useGetPostsQuery } from '../../shared/api';
 import Post from 'entities/Post';
 import { toast } from 'react-toastify';
@@ -8,6 +8,8 @@ import { FixedSizeList as List } from 'react-window';
 import { ListChildComponentProps } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
+//FIRST EXAMPLE of virtual scroll with react-window lib
+//Second one is below
 function PostsList() {
   const listRef = useRef<List>(null!);
   const { data, isError, isLoading } = useGetPostsQuery();
@@ -29,7 +31,7 @@ function PostsList() {
   };
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div className={styles.wrapper}>
       {isLoading && <Spin className={styles.spin} />}
       {posts && (
         <AutoSizer>
@@ -51,3 +53,81 @@ function PostsList() {
 }
 
 export default PostsList;
+
+//SECOND EXAMPLE of virtual scroll
+
+// function PostsList() {
+//   const { data, isError, isLoading } = useGetPostsQuery();
+//   const posts = data ?? [];
+//   const limit = 10;
+//   const rowHeight = 76;
+//   const totalCount = posts.length;
+//   const maxScroll = posts.length * rowHeight - limit * rowHeight;
+//   const [scrollTop, setScrollTop] = useState(0);
+//   const startNode = Math.ceil(scrollTop / rowHeight);
+//   const visibleData = posts.slice(startNode, startNode + limit);
+//   const startRowHeight = scrollTop;
+//   const endRowHeight = posts.length * rowHeight - startRowHeight - limit * rowHeight;
+
+//   const lastRowRef = useRef<HTMLDivElement>(null);
+//   const scrollerRef = useRef<HTMLDivElement>(null);
+//   const firstRowRef = useRef<HTMLDivElement>(null);
+
+//   useEffect(() => {
+//     if (isError) {
+//       toast('Oops, something went wrong!');
+//     }
+//   }, [isError]);
+
+//   const scrollHandler = (): void => {
+//     if (lastRowRef.current !== null && scrollerRef.current !== null) {
+//       const lastRowRefRect = lastRowRef.current.getBoundingClientRect();
+//       const scrollerRefRect = scrollerRef.current.getBoundingClientRect();
+//       const firstRowRefRect = firstRowRef.current?.getBoundingClientRect();
+
+//       if (
+//         lastRowRefRect.top < scrollerRefRect.bottom &&
+//         visibleData[visibleData.length - 1].id !== totalCount
+//       ) {
+//         setScrollTop(scrollerRef.current.scrollTop - rowHeight);
+//       }
+
+//       if (firstRowRefRect!.top > scrollerRefRect.top && scrollerRef.current.scrollTop !== 0) {
+//         setScrollTop(scrollerRef.current.scrollTop - rowHeight);
+//       }
+
+//       if (scrollerRef.current.scrollTop === 0) {
+//         setScrollTop(0);
+//       }
+
+//       if (scrollerRef.current.scrollTop >= maxScroll) {
+//         setScrollTop(maxScroll);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{ height: `${limit * rowHeight - rowHeight * 2}px`, overflow: 'auto' }}
+//       onScroll={scrollHandler}
+//       ref={scrollerRef}
+//     >
+//       <div style={{ height: startRowHeight }}></div>
+
+//       {isLoading && <Spin className={styles.spin} />}
+//       {posts &&
+//         visibleData.map((post, idx) => (
+//           <div
+//             ref={idx === visibleData.length - 1 ? lastRowRef : idx === 0 ? firstRowRef : null}
+//             key={post.id}
+//           >
+//             <Post post={post} />
+//           </div>
+//         ))}
+
+//       <div style={{ height: endRowHeight }}></div>
+//     </div>
+//   );
+// }
+
+// export default PostsList;
